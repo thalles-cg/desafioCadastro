6f1535635fa110ad9d44afd44f7d03a71beb0db8
thalles-cg <thalles.pity@gmail.com>
2025-06-13 20:18:25 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import dominio.Animal;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Animal animal = new Animal("Alfred");
+        System.out.println(animal.getNome());
+
+    }
+}
---
+++ src/dominio/Animal.java
@@ -0,0 +1,17 @@
+package dominio;
+
+public class Animal {
+    private String nome;
+
+    public Animal(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
