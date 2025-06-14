e6813582817eaacc575475cb08dca6eeafca3112
thalles-cg <thalles.pity@gmail.com>
2025-06-13 21:09:49 -0300

    feat: Menu de escolha melhorado

--- src/Main.java
+++ src/Main.java
@@ -7,23 +7,24 @@ public class Main {
 
         Scanner in = new Scanner(System.in);
         int resposta = 1;
-        while(resposta != 6){
+        while(true){
             System.out.println("1. Cadastrar um novo pet\n" +
                     "2. Alterar os dados do pet cadastrado\n" +
                     "3. Deletar um pet cadastrado\n" +
                     "4. Listar todos os pets cadastrados\n" +
                     "5. Listar pets por algum critério (idade, nome, raça)\n" +
                     "6. Sair");
-            System.out.print("Resposta: ");
+            System.out.print("-----------------------------------\nResposta: ");
             resposta = in.nextInt();
             if (resposta == 6){
-                continue;
+                break;
             } else if(resposta < 1 || resposta > 5) {
                 System.out.println("não mostrou");
                 continue;
             }
             System.out.println("mostrou");
         }
+        System.out.print("=== ENCERRANDO PROGRAMA ===");
         in.close();
 
     }
