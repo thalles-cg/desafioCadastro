1c4eb8956428e610cc5f31b60c4329b302a35097
thalles-cg <thalles.pity@gmail.com>
2025-06-13 21:05:50 -0300

    feat: Adição do menu de escolha

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,30 @@
 import dominio.Animal;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        Animal animal = new Animal("Alfred");
-        System.out.println(animal.getNome());
+        Scanner in = new Scanner(System.in);
+        int resposta = 1;
+        while(resposta != 6){
+            System.out.println("1. Cadastrar um novo pet\n" +
+                    "2. Alterar os dados do pet cadastrado\n" +
+                    "3. Deletar um pet cadastrado\n" +
+                    "4. Listar todos os pets cadastrados\n" +
+                    "5. Listar pets por algum critério (idade, nome, raça)\n" +
+                    "6. Sair");
+            System.out.print("Resposta: ");
+            resposta = in.nextInt();
+            if (resposta == 6){
+                continue;
+            } else if(resposta < 1 || resposta > 5) {
+                System.out.println("não mostrou");
+                continue;
+            }
+            System.out.println("mostrou");
+        }
+        in.close();
 
     }
 }
